d79e3371483ec430e40f4476f3cb10951909b46b
navee <naveenarameshvardhini@gmail.com>
2022-01-13 17:20:56 +0530

    fix:Display API

--- networking_flutter/lib/detail.dart
+++ networking_flutter/lib/detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:networking_flutter/model/git_api.dart';
 
 class Detail extends StatefulWidget {
@@ -18,29 +19,210 @@ class _DetailState extends State<Detail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Text(widget.githubApi.email,
-            //     style: const TextStyle(
-            //       color: Colors.white,
-            //     )),
-            Text(widget.githubApi.bio,
-                style: const TextStyle(
-                  color: Colors.white,
-                )),
-            Text(widget.githubApi.location,
-                style: const TextStyle(
-                  color: Colors.white,
-                )),
-            Text(widget.githubApi.name,
-                style: const TextStyle(
-                  color: Colors.white,
-                )),
-          ],
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(
+            Icons.arrow_back_ios_outlined,
+            color: Colors.white,
+            size: 18,
+          ),
         ),
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Text(
+          widget.githubApi.name,
+          style: GoogleFonts.lato(
+              color: Colors.white, fontWeight: FontWeight.normal),
+        ),
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, top: 30),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 60,
+                  backgroundImage: NetworkImage(widget.githubApi.avatarUrl),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 10.0,
+                  ),
+                  child: Container(
+                    constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width / 1.7),
+                    child: Column(
+                      children: [
+                        Text(
+                          widget.githubApi.bio,
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.lato(
+                              color: Colors.white70,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 14),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  "Followers",
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.lato(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                ),
+                                Text(
+                                  widget.githubApi.followers.toString(),
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.lato(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  "Following ",
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.lato(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                ),
+                                Text(
+                                  widget.githubApi.following.toString(),
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.lato(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Expanded(
+              child: Container(
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+                color: Colors.white12),
+            child: Column(
+              children: [
+                ListTile(
+                    title: Padding(
+                      padding: const EdgeInsets.only(top: 20.0, left: 20),
+                      child: Text("Location",
+                          style: GoogleFonts.lato(
+                              color: Colors.white, fontSize: 18)),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(left: 20.0, top: 8),
+                      child: Text(
+                        widget.githubApi.location,
+                        style:
+                            GoogleFonts.lato(color: Colors.grey, fontSize: 14),
+                      ),
+                    )),
+                ListTile(
+                    title: Padding(
+                      padding: const EdgeInsets.only(top: 20.0, left: 20),
+                      child: Text("Company",
+                          style: GoogleFonts.lato(
+                              color: Colors.white, fontSize: 18)),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(left: 20.0, top: 8),
+                      child: Text(
+                        widget.githubApi.company,
+                        style:
+                            GoogleFonts.lato(color: Colors.grey, fontSize: 14),
+                      ),
+                    )),
+                ListTile(
+                    title: Padding(
+                      padding: const EdgeInsets.only(top: 20.0, left: 20),
+                      child: Text("Twitter",
+                          style: GoogleFonts.lato(
+                              color: Colors.white, fontSize: 18)),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(left: 20.0, top: 8),
+                      child: Text(
+                        widget.githubApi.twitterUsername,
+                        style:
+                            GoogleFonts.lato(color: Colors.grey, fontSize: 14),
+                      ),
+                    )),
+                ListTile(
+                    title: Padding(
+                      padding: const EdgeInsets.only(top: 20.0, left: 20),
+                      child: Text("Blog",
+                          style: GoogleFonts.lato(
+                              color: Colors.white, fontSize: 18)),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(left: 20, top: 8),
+                      child: Text(
+                        widget.githubApi.blog,
+                        style:
+                            GoogleFonts.lato(color: Colors.grey, fontSize: 14),
+                      ),
+                    )),
+                ListTile(
+                    title: Padding(
+                      padding: const EdgeInsets.only(top: 20.0, left: 20),
+                      child: Text("Organisation",
+                          style: GoogleFonts.lato(
+                              color: Colors.white, fontSize: 18)),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(left: 20, top: 8),
+                      child: Text(
+                        widget.githubApi.organizationsUrl,
+                        style:
+                            GoogleFonts.lato(color: Colors.grey, fontSize: 14),
+                      ),
+                    )),
+                ListTile(
+                    title: Padding(
+                      padding: const EdgeInsets.only(top: 20.0, left: 20),
+                      child: Text("Repos",
+                          style: GoogleFonts.lato(
+                              color: Colors.white, fontSize: 18)),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(left: 20, top: 8),
+                      child: Text(
+                        widget.githubApi.reposUrl,
+                        style:
+                            GoogleFonts.lato(color: Colors.grey, fontSize: 14),
+                      ),
+                    )),
+              ],
+            ),
+          ))
+        ],
       ),
     );
   }
